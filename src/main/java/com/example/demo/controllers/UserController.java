7497970a2b1ad7a0aceaecf3cee85fca88b24aef
kalenduhs <kalenduhs@gmail.com>
2024-06-06 20:53:32 -0500

    user creation and login

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controllers;
+
+import com.example.demo.dto.UserDto;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto createdUser = userService.createUser(userDto);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+}
